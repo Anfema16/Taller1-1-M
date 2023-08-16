a7f12e64a3eaa2ebc23b3c83d23c3b4fd7afde04
Andres <141367266+Anfema16@users.noreply.github.com>
2023-08-16 12:22:09 -0500

    Commit 4

---
+++ Operacion/src/main/java/com/mycompany/operacion/Operacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacion;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Operacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
